5af5df43ec8a6b740f0325a957107655998a429d
BaeKoR <inser98@naver.com>
2023-04-12 09:37:28 +0900

    first commit

---
+++ src/test/java/mul/cam/a/MineApplicationTests.java
@@ -0,0 +1,13 @@
+package mul.cam.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
