5496f355400c879d19db8e726fed16cd5b3a8d5b
taewopark <118885766+taewopark@users.noreply.github.com>
2023-04-12 18:05:07 +0900

    Update 8bit.sql

--- src/main/8bit.sql
+++ src/main/8bit.sql
@@ -4,6 +4,63 @@ create database mine;
 
 -- mine DB 사용
 use mine;
+-----------------------태우----------------------------------
+CREATE TABLE MY_FILEIOAD(
+	MF_SEQ INT AUTO_INCREMENT PRIMARY KEY, -- 파일 번호
+	MF_CATEGORY VARCHAR(10),  --카테고리
+	MF_TITLE VARCHAR(200) NOT NULL, --  파일명
+	MF_REGDATE TIMESTAMP DEFAULT NOW() NOT NULL, -- 작성일
+	MF_MEMO  VARCHAR(200), -- 메모
+	MF_FILE_ID VARCHAR(50) NOT NULL, -- 작성자	
+	MF_FILENAME DECIMAL(10), --원본파일 
+	MF_NEWFILENAME DECIMAL(10), -- 담아둘거
+	REFERENCES MEMBER()  ON DELETE CASCADE,
+);
+
+
+CREATE TABLE GUEST_BOOK(
+    GB_SEQ INT AUTO_INCREMENT PRIMARY KEY, -- 글 번호
+    GB_ID VARCHAR(50) NOT NULL, -- 작성자
+    GB_CONTENT VARCHAR(4000) NOT NULL, -- 글 내용
+    GB_WRITEDATE TIMESTAMP DEFAULT NOW() NOT NULL, -- 작성일
+   REFERENCES MEMBER()  ON DELETE CASCADE,
+);
+
+
+CREATE TABLE GUEST_COMMENT(
+ 	GC_SEQ INT, -- 댓글이 작성될 글의 번호
+	GC_COMMENTSEQ INT AUTO_INCREMENT PRIMARY KEY, -- 댓글 고유 번호
+    	GC_REF DECIMAL(8) NOT NULL,
+   	GC_DEPTH DECIMAL(1) DEFAULT 0,
+   	GC_STEP DECIMAL(8) DEFAULT 0, 
+	GC_ID VARCHAR(50),
+	GC_CONTENT VARCHAR(4000) NOT NULL, -- 댓글 내용
+	GC_WRITEDATE TIMESTAMP NOT NULL, -- 댓글 작성 일자
+	REFERENCES GB_FRIEND(GB_ID)  ON DELETE CASCADE,
+);
+
+
+CREATE TABLE GB_CARD(
+	GBC_SEQ INT AUTO_INCREMENT PRIMARY KEY, -- 명함번호
+	GBC_CATEGORY VARCHAR(10) --카테고리
+	GBC_REGDATE TIMESTAMP DEFAULT NOW() NOT NULL, -- 작성일
+	GBC_MEMO  VARCHAR(200), -- 메모
+	GBC_ID VARCHAR(50) NOT NULL, -- 명함이름	
+	REFERENCES MEMBER()  ON DELETE CASCADE,
+);
+
+
+CREATE TABLE IN_INSTT(
+IN_SEQ INT AUTO_INCREMENT PRIMARY KEY, -- 번호
+IN_ID VARCHAR(50) NOT NULL, -- 이름		
+IN_REGDATE TIMESTAMP DEFAULT NOW() NOT NULL, -- 작성일
+IN_MEMO  VARCHAR(200), -- 메모
+IN_TEMP  VARCHAR(200), -- 음성담기
+);
+
+
+
+-----------------------태우----------------------------------
 
 -- -------------------다연 ------------------------------------------------------------------------------------
 -- Me(다이어리) DB -----------------------------------------------------------------
