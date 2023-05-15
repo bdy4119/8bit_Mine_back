b4e30e683604970697b5298e2b61c56b97021b84
subin1016 <subin1016@naver.com>
2023-05-15 11:15:20 +0900

    subin0515

--- src/main/java/mul/cam/a/dto/MineAnswerDto.java
+++ src/main/java/mul/cam/a/dto/MineAnswerDto.java
@@ -7,6 +7,9 @@ public class MineAnswerDto implements Serializable{
 	private int seq;
 	private String userid;
 	private String mineid;
+	private String question1;
+	private String question2;
+	private String question3;
 	private String answer1;
 	private String answer2;
 	private String answer3;
@@ -15,11 +18,15 @@ public class MineAnswerDto implements Serializable{
 		
 	}
 
-	public MineAnswerDto(int seq, String userid, String mineid, String answer1, String answer2, String answer3) {
+	public MineAnswerDto(int seq, String userid, String mineid, String question1, String question2, String question3,
+			String answer1, String answer2, String answer3) {
 		super();
 		this.seq = seq;
 		this.userid = userid;
 		this.mineid = mineid;
+		this.question1 = question1;
+		this.question2 = question2;
+		this.question3 = question3;
 		this.answer1 = answer1;
 		this.answer2 = answer2;
 		this.answer3 = answer3;
@@ -49,6 +56,30 @@ public class MineAnswerDto implements Serializable{
 		this.mineid = mineid;
 	}
 
+	public String getQuestion1() {
+		return question1;
+	}
+
+	public void setQuestion1(String question1) {
+		this.question1 = question1;
+	}
+
+	public String getQuestion2() {
+		return question2;
+	}
+
+	public void setQuestion2(String question2) {
+		this.question2 = question2;
+	}
+
+	public String getQuestion3() {
+		return question3;
+	}
+
+	public void setQuestion3(String question3) {
+		this.question3 = question3;
+	}
+
 	public String getAnswer1() {
 		return answer1;
 	}
@@ -75,8 +106,9 @@ public class MineAnswerDto implements Serializable{
 
 	@Override
 	public String toString() {
-		return "MineAnswerDto [seq=" + seq + ", userid=" + userid + ", mineid=" + mineid + ", answer1=" + answer1
-				+ ", answer2=" + answer2 + ", answer3=" + answer3 + "]";
+		return "MineAnswerDto [seq=" + seq + ", userid=" + userid + ", mineid=" + mineid + ", question1=" + question1
+				+ ", question2=" + question2 + ", question3=" + question3 + ", answer1=" + answer1 + ", answer2="
+				+ answer2 + ", answer3=" + answer3 + "]";
 	}
 
 }
